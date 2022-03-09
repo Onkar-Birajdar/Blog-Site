ad13ab45a2eb73d87466fe30b96a4dd0d16fcead
Onkar Birajdar <67547781+Onkar-Birajdar@users.noreply.github.com>
2022-03-10 00:26:40 +0530

    Getting files back

--- app.js
+++ app.js
@@ -1,46 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const _ = require("lodash");
 const ejs = require("ejs");
 
 const port = process.env.PORT || 3000;
 const app = express();
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/blogDB", {
-    useNewUrlParser: true
+    useNewUrlParser: true,
 });
 
-const blogPostSchema = mongoose.Schema({
-    title: String,
-    body: String
+const blogPostSchema = new mongoose.Schema({
+    heading: String,
+    content: String,
 });
 
-const BlogPost = mongoose.model("BlogPost", blogPostSchema);
-
-const blogPost1 = new BlogPost({
-    title: "Blog Post 1",
-    body: "This is the first blog post"
-});
-const blogPost2 = new BlogPost({
-    title: "Blog Post 2",
-    body: "This is the first blog post",
-});
-const blogPost3 = new BlogPost({
-    title: "Blog Post 3",
-    body: "This is the first blog post",
-});
-const blogPost4 = new BlogPost({
-    title: "Blog Post 4",
-    body: "This is the first blog post",
-});
-
-
+const BlogPost = new mongoose.model("BlogPost", blogPostSchema);
 
 const homeStartingContent =
     "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -49,35 +28,45 @@ const aboutContent =
 const contactContent =
     "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+const blogPost1 = new BlogPost({
+    heading: "First Post",
+    content:
+        "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.",
+});
+const blogPost2 = new BlogPost({
+    heading: "Second Post",
+    content:
+        "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.",
+});
+const blogPost3 = new BlogPost({
+    heading: "Third Post",
+    content:
+        "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.",
+});
+const blogPost4 = new BlogPost({
+    heading: "Fourth Post",
+    content:
+        "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.",
+});
 
-
-let blogPosts = [
-    {
-        heading: "First Post",
-        content:
-            "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.",
-    },
-    {
-        heading: "Second Post",
-        content:
-            "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.",
-    },
-    {
-        heading: "Third Post",
-        content:
-            "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.",
-    },
-    {
-        heading: "Fourth Post",
-        content:
-            "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.",
-    },
-];
-
+const defaultPosts = [blogPost1, blogPost2, blogPost3, blogPost4];
 //root
 app.get("/", (req, res) => {
-    res.render("home", {
-        posts: blogPosts,
+    BlogPost.find({}, (err, foundItems) => {
+        if (foundItems.length === 0) {
+            BlogPost.insertMany(defaultPosts, (err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Successfully inserted data");
+                }
+            });
+            res.redirect("/");
+        } else {
+            res.render("home", {
+                posts: foundItems,
+            });
+        }
     });
 });
 //home
@@ -92,7 +81,10 @@ app.get("/about.ejs", (req, res) => {
 
 //Contact
 app.get("/contact.ejs", (req, res) => {
-    res.render("contact", { heading: "Contact Us", content: contactContent });
+    res.render("contact", {
+        heading: "Contact Us",
+        content: contactContent,
+    });
 });
 
 // compose
@@ -103,16 +95,21 @@ app.get("/compose.ejs", (req, res) => {
 app.post("/compose.ejs", (req, res) => {
     let heading = req.body.postHeading;
     let content = req.body.postContent;
-    blogPosts.push({ heading, content });
+    const newBlogPost = new BlogPost({
+        heading: heading,
+        content: content,
+    });
+    newBlogPost.save();
     res.redirect("/");
 });
 
 app.get("/post/:postId", (req, res) => {
-    let title = (req.params.postId);
-    let item = blogPosts.filter((item) => item.heading.includes(title));
-    res.render("post", {
-        heading: item[0].heading,
-        postContent: item[0].content,
+    let requestedPostId = req.params.postId;
+    BlogPost.findOne({ _id: requestedPostId }, function (err, post) {
+        res.render("post", {
+            heading: post.heading,
+            postContent: post.content,
+        });
     });
 });
 
